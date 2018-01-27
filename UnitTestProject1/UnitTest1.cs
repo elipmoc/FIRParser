d5e5909bd42e515d0ef5dcc437c9707b1bbf5fd1
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-27 22:37:15 +0900

    [add]テストフレームワーク追加

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FIRParser;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Parser.Parse("output 111");
+        }
+    }
+}
