7c40c5287a7f57674d5802e673b99b1dd7f96b62
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-27 23:15:13 +0900

    [add]AstParser作成

---
+++ FIRParser/AstParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIRParser
+{
+    public static class AstParser
+    {
+        public static Ast.OpCodeAst Parse(string str)
+        {
+            return new Ast.OpCodeAst();
+        }
+    }
+}
