53f7f569c53ed10c2c8e95de9b75d92f51f44f4b
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-01-28 16:47:14 +0900

    [add]ParseResultを作成

---
+++ FIRParser/ParseResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIRParser.Ast;
+
+namespace FIRParser
+{
+    //Parseした結果の型
+    class ParseResult
+    {
+        public ParseResult(BaseAst entryAst,bool successFlag,List<string> errorMsgList) {
+        }
+    }
+}
